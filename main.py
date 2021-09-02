a105e22bbfc208739ec3d1573a0a01ce74683ea8
ismailpervez <freelanceismail001@gmail.com>
2021-09-02 16:57:03 +0300

    added a simple credentials class for the credentials and maded a simple test

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 A simple application to store passwords
 """
 
+class Credential:
+    def __init__(self, accountName, password):
+        self.accountName = accountName
+        self.password = password
+
 # user class
 class User:
     def __init__(self, username, password):
@@ -11,4 +16,6 @@ class User:
 
 
 user1 = User("ismailpervez", "password123")
+cred1 = Credential("twitter", "password001")
 print(user1.username)
+print(cred1.accountName)
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # password locker (python)
 a simple command line based application that enables users to store their credentials securely
 
+# description
+this is a simple command line based application developed using the python programming language. this app is developed by [pervez nagi](https://github.com/ismailpervez) 
