8a04aeccb67bd90221dcbfec2dcdbe3af100c8be
ismailpervez <freelanceismail001@gmail.com>
2021-09-05 20:42:40 +0300

    added a User class

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ class Credential:
     def __init__(self, accountName, password):
         self.accountName = accountName
         self.password = password
-
-# user class
+"""
+class User is used as a blueprint for a user
+a user is required to have a username and a password to access his/her credentials
+a user also has a list containing saved credentials
+"""
 class User:
+    users_credentials = []
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
 
-# user1 = User("ismailpervez", "password123")
-# cred1 = Credential("twitter", "password001")
-# print(user1.username)
-# print(cred1.accountName)
+
