ab86c819f174edad8ebae1ba1412d84dcb04b486
ismailpervez <freelanceismail001@gmail.com>
2021-09-05 20:46:29 +0300

    added a method in user class to get all credentials stored

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Credential:
 """
 class User is used as a blueprint for a user
 a user is required to have a username and a password to access his/her credentials
-a user also has a list containing saved credentials
+a user also has a list containing saved
 """
 class User:
     users_credentials = []
@@ -18,5 +18,15 @@ class User:
         self.username = username
         self.password = password
 
+    def get_creds(self, user_input_password):
+        if (user_input_password == self.password):
+            #print(self.users_credentials)
+            if len(self.users_credentials) == 0:
+                print("no password saved yet")
 
+            for cred in self.users_credentials:
+                print(f"password for {cred.account_name} account is {cred.account_password}")
+
+        else:
+            print("wrong password!")
 
